21cff189b842de52cefae4a44a4936e27e165d20
Alden Davidson <adavidson+github@protonmail.ch>
2021-12-10 15:57:35 -0800

    fixed reference pointing to invalid column

--- zprefix-blog-server/migrations/20211210043433_posts_table.js
+++ zprefix-blog-server/migrations/20211210043433_posts_table.js
@@ -1,7 +1,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable("posts", function (table) {
     table.increments("id");
-    table.integer("author_id").references("user_id").inTable("users");
+    table.integer("author_id").references("id").inTable("users");
     table.string("title").notNullable();
     table.text("content").notNullable();
     table.timestamp("created_at").defaultTo(knex.fn.now());
