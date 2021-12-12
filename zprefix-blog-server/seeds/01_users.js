a32f566a4ed1aa3d093c9356adae81a3b464dd3d
Alden Davidson <adavidson+github@protonmail.ch>
2021-12-11 20:17:49 -0800

    updated user seeds to use hashed passwords

--- zprefix-blog-server/seeds/01_users.js
+++ zprefix-blog-server/seeds/01_users.js
@@ -5,9 +5,21 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex("users").insert([
-        { username: "S3L4phiel", password: "v49" },
-        { username: "D0G", password: "v55" },
-        { username: "Sheep", password: "v69" },
+        {
+          username: "S3L4phiel",
+          hashed_password:
+            "$2b$10$mgiNEZe/U8XjiUltxdn3p.kxfcLFRri4lQnEExSNSs54thTJZuSWK",
+        }, // password = v49
+        {
+          username: "D0G",
+          hashed_password:
+            "$2b$10$pmGOt.3xT5vZWkgQVgltVOxWgdeM2Tbdz833avGenDkaWuzWDTWnu",
+        }, // password = v55
+        {
+          username: "Sheep",
+          hashed_password:
+            "$2b$10$H53TCGNZiw7/NPHP2PuFxeZt.kVXk5jWcxl.BxPg8eJyQ7Xa8p.5S",
+        }, // password = v69
       ]);
     });
 };
