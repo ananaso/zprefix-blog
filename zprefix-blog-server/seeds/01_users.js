3a43bcefaa618a79123320ff82d7386a4b6bad94
Alden Davidson <adavidson+github@protonmail.ch>
2021-12-11 18:42:54 -0800

    removed id to allow proper autoincrementing

--- zprefix-blog-server/seeds/01_users.js
+++ zprefix-blog-server/seeds/01_users.js
@@ -5,9 +5,9 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex("users").insert([
-        { id: 1, username: "S3L4phiel", password: "v49" },
-        { id: 2, username: "D0G", password: "v55" },
-        { id: 3, username: "Sheep", password: "v69" },
+        { username: "S3L4phiel", password: "v49" },
+        { username: "D0G", password: "v55" },
+        { username: "Sheep", password: "v69" },
       ]);
     });
 };
