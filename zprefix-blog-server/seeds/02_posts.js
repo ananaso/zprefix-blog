5b319ce1b6502b46c0520940677402699e540fb7
Alden Davidson <adavidson+github@protonmail.ch>
2021-12-13 19:20:02 -0800

    removed id to allow seeding to autoincrement

--- zprefix-blog-server/seeds/02_posts.js
+++ zprefix-blog-server/seeds/02_posts.js
@@ -6,28 +6,24 @@ exports.seed = function (knex) {
       // Inserts seed entries
       return knex("posts").insert([
         {
-          id: 1,
           author_id: 3,
           title: "Where are we?",
           content:
             "I don't know what this place is, and nor it seems does anyone else. Nothing to do but move forward and note whatever I find.",
         },
         {
-          id: 2,
           author_id: 2,
           title: "What's the point?",
           content:
             "Okay, so I get that I'm supposed to retrieve these sigils. I just don't get WHY. Sure the loud voice is telling me to, but this world of his has no context, no purpose, and no foundations from which to construct them.",
         },
         {
-          id: 3,
           author_id: 3,
           title: "Archive or design?",
           content:
             "I can't tell if the documents on the terminals are all that's left from a larger archive, or are carefully designed to communicate some hidden truth.",
         },
         {
-          id: 4,
           author_id: 1,
           title: "Seeking secrets",
           content:
