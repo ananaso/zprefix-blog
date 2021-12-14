e4c677ba8bb1ed69643fe5dd883dd2dcb833a8e6
Alden Davidson <adavidson+github@protonmail.ch>
2021-12-13 20:39:43 -0800

    reduced timestamps displayed to just date, to better match user stories

--- zprefix-blog-app/src/components/BlogPost.jsx
+++ zprefix-blog-app/src/components/BlogPost.jsx
@@ -1,15 +1,12 @@
 const BlogPost = ({ postInfo }) => {
   const { title, username, content } = postInfo;
   const posted = new Date(Date.parse(postInfo.created_at));
-  const updated = new Date(Date.parse(postInfo.updated_at));
   return (
     <article className="BlogPost">
       <header>
         <h1>{title}</h1>
         <address>Author: {username}</address>
-        <time>Posted: {posted.toLocaleString()}</time>
-        <br />
-        <time>Last Updated: {updated.toLocaleString()}</time>
+        <time>Posted: {posted.toLocaleDateString()}</time>
       </header>
       <p>
         {content.length > 100 ? `${content.slice(0, 100)}...` : `${content}`}
