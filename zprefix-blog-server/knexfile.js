da5376229bf742fd3e8f1e8af6fbaf510cbd6487
Alden Davidson <adavidson+github@protonmail.ch>
2021-12-18 10:05:34 -0800

    updating knexfile for production

--- zprefix-blog-server/knexfile.js
+++ zprefix-blog-server/knexfile.js
@@ -18,17 +18,10 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
     },
   },
 };
