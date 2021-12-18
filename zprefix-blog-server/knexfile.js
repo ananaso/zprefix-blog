276f941d900eb27f460cc56702b20a548403617f
Alden Davidson <adavidson+github@protonmail.ch>
2021-12-18 11:24:21 -0800

    ugh why can't I get rid of this ssl crap

--- zprefix-blog-server/knexfile.js
+++ zprefix-blog-server/knexfile.js
@@ -19,13 +19,12 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
-    // {
-    //   connectionString: process.env.DATABASE_URL,
-    //   ssl: {
-    //     rejectUnauthorized: false,
-    //   },
-    // },
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
     pool: {
       min: 2,
       max: 10,
